5c4c20f68054f671922d2358063a1f78c25b704c
Robert Cloa <8luM3CT0@users.noreply.github.com>
2021-09-22 06:32:40 -0700

    new commit again cause of chat errors

--- pages/_app.js
+++ pages/_app.js
@@ -3,19 +3,44 @@ import 'tailwindcss/tailwind.css'
 import 'react-dropdown/style.css'
 import Router from 'next/router'
 import ProgressBar from '@badrap/bar-of-progress'
-
-const progress = new ProgressBar({
-  size: 4,
-  color: '#bfdbfe',
-  className: 'z-50',
-  delay: 100
-})
-
-Router.events.on('routeChangeStart', progress.start)
-Router.events.on('routeChangeComplete', progress.finish)
-Router.events.on('routeChangeError', progress.finish)
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { userCreds, store } from '../firebase'
+import { useEffect } from 'react'
+import firebase from 'firebase'
+import Home from './index'
 
 function MyApp ({ Component, pageProps }) {
+  const [user] = useAuthState(userCreds)
+  useEffect(() => {
+    if (user) {
+      store
+        .collection('users')
+        .doc(user.uid)
+        .set(
+          {
+            email: user.email,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+            lastSeen: firebase.firestore.FieldValue.serverTimestamp()
+          },
+          { merge: true }
+        )
+    }
+  }, [user])
+
+  const progress = new ProgressBar({
+    size: 4,
+    color: '#bfdbfe',
+    className: 'z-50',
+    delay: 100
+  })
+
+  Router.events.on('routeChangeStart', progress.start)
+  Router.events.on('routeChangeComplete', progress.finish)
+  Router.events.on('routeChangeError', progress.finish)
+
+  if (!user) return <Home />
+
   return <Component {...pageProps} />
 }
 
